1583a2968126245f767385424cdffcea0d2ca37d
wonjiyang <wonjiyang@gmail.com>
2024-02-23 15:51:35 +0900

    add

---
+++ dist/js/comm.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+    $("#fullPage").fullpage({
+        // autoScrolling: true,
+        // scrollHorizontally: true,
+        navigation: true,
+        showActiveTooltip: true,
+        anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage", "fifthPage", "sixthPage", "sevenPage", "eightPage"],
+        afterLoad: function (anchorLink, index) {
+            if (index == 1) {
+                $(".home-wrap").css({
+                    width: "450px",
+                    height: "450px",
+                });
+                $(".home-wrap").animate(
+                    {
+                        opacity: 1,
+                    },
+                    100
+                );
+                $(".arrow").css({
+                    opacity: 1,
+                });
+            }
+        },
+        
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 1 && direction == "down") {
+                $(".home-wrap").css({
+                    width: "4000px",
+                    height: "2000px",
+                });
+                $(".arrow").css({
+                    opacity: 0,
+                });
+            }
+        },
+    });
+
+    // 마우스커서
+    var $mousePointer = $("#mouse-pointer"),
+        $clickElements = $("a, label");
+
+    function moverCursor(e) {
+        $mousePointer.css({
+            left: e.pageX,
+            top: e.pageY,
+        });
+    }
+    $clickElements.mouseenter(function () {
+        $mousePointer.addClass("hover");
+    });
+    $clickElements.mouseleave(function () {
+        $mousePointer.removeClass("hover");
+    });
+
+    $(window).on("mousemove", moverCursor);
+
+    $(".app-form-button").on("click", function () {
+        swal({
+            title: "이메일 전송완료!🤗",
+            icon: "success",
+        });
+    });
+
+    const menu = document.querySelector('.headerview-menu');
+    const menuList = document.querySelector('.headerview-list');
+
+    menu.addEventListener('click', function(e) {
+        e.preventDefault();
+        menu.classList.toggle('add');
+        menuList.classList.toggle('show');
+    });
+});
+          
+
